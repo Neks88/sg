b51384dd2edb4cb890c9e010af13a5f11b91bdcb
neks <neks.92@live.com>
2019-03-27 20:38:11 +0100

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title:'Javascript: The Good Parts'},
+        {title:'Harry Poter'},
+        {title:'The Dark Tower'},
+        {title:'Eloquent Ruby'}
+    ]
+}
